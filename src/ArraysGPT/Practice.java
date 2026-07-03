273d61d54d41ea59ecadffe0c419efc59f85c572
Sourabh <sourabhdhole83@gmail.com>
2026-07-03 12:51:06 +0530

    sfd

--- src/ArraysGPT/Practice.java
+++ src/ArraysGPT/Practice.java
@@ -3,5 +3,13 @@ package ArraysGPT;
 public class Practice {
     public static void main(String[] args) {
 
+        int[] arr = {1,1,2,3,3,4};
+
+       int largest = Integer.MIN_VALUE;
+       int seclar = Integer.MIN_VALUE;
+
+       for(int i=0;i<arr.length;i++){
+
+       }
     }
 }
--- src/ArraysGPT/Ques.java
+++ src/ArraysGPT/Ques.java
@@ -2,7 +2,7 @@ package ArraysGPT;
 
 public class Ques {
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4, 5, 4, 5};
+//        int[] arr = {1, 2, 3, 4, 5, 4 , 7};
 
         //reverse the array
 //         int left = 0;
@@ -24,8 +24,8 @@ public class Ques {
 //===========find largest============
 
 //        int max = 0;
-//        for(int i =0;i<a.length-1;i++){
-//            if(temp<a[i]){
+//        for(int i =0;i<a.length;i++){
+//            if(max<a[i]){
 //                max = a[i];
 //                i++;
 //            }
@@ -35,15 +35,42 @@ public class Ques {
 
 //=============2nd largest============
 
-        int max = Integer.MIN_VALUE;
-        int secMax=Integer.MIN_VALUE;
-        for(int num:a){
-
-            if(num>max){
-                max=num;
-            }
+//        int max = Integer.MIN_VALUE;
+//        int secMax=Integer.MIN_VALUE;
+//        for(int num:a){
+//
+//            if(num>max){
+//                max=num;
+//            }
 //            else if(num>max )
+
+
+//=================Check Array is Sorted=========================
+
+//        boolean sorted = true;
+//        for(int i = 0;i< arr.length-1;i++){
+//            if(arr[i]>arr[i+1]){
+//                sorted = false;
+//                break;
+//            }
+//        }
+//        System.out.println(sorted);
+
+//==================Remove Duplicates from Sorted Array==================================
+
+        int[] nums = {1,1,2,2,3,4,4};
+
+        int i = 0; // Points to the last unique element
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        System.out.println(i + 1);
+
         }
 
     }
-}
